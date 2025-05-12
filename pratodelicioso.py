d9b7e267e655233e5411e75869685c3d5c5d0858
Gui6404 <guilhermegomes331@gmail.com>
2025-05-12 19:27:01 -0300

    Add files via upload

---
+++ pratodelicioso.py
@@ -0,0 +1,37 @@
+import random
+
+print(30*"+")
+print("DESCUBRA O PRATO DELICIOSO")
+print(30*"+")
+
+pratos = ["Feijoada","Lasanha","Churrasco","Moqueca","Estrogonofe","Frango à parmegiana","Bobó de camarão","Tacos","Pizza","Sushi","Yakissoba","Risoto de cogumelos","Hambúrguer artesanal","Ceviche","Cuscuz nordestino","Nhoque ao sugo","Paella","Macarronada","Carne de sol com mandioca","Ratatouille"]
+
+prato_secreto = random.choice(pratos)
+print(prato_secreto)
+nivel = int(input("Escolha entre as dificuldades \nFácil (digite 1) \nMédio (digite 2) \nDifícil (digite 3): "))
+
+if nivel == 1:
+    jogadas = 30
+    perda_serro = 10
+    print("Você terá {} jogadas".format(jogadas))
+elif nivel == 2:
+    jogadas = 10
+    perda_serro = 20
+    print("Você terá {} jogadas".format(jogadas))
+elif nivel == 3:
+    jogadas = 5
+    perda_serro = 50
+    print("Você terá {} jogadas".format(jogadas))
+else:
+    print("Escolha corretamente sua dificuldade de jogo")
+
+
+
+for jogada in range(1, jogadas+1): 
+    palpite = str(input("Qual o prato delicioso secreto ? "))
+    if palpite == prato_secreto:
+        print("Parabéns você acertou o prato secreto! {} é delicioso mesmo!".format(palpite))
+        break
+    else:
+        restam = (jogadas) - jogada
+        print("Você errou! Restam {} tentativas".format(restam))
